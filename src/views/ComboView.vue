a7d0bcbfb940d744f45f814a7e74c6048accbf37
tosaki <t.chay9271@gmail.com>
2025-03-24 14:50:24 +0900

    fix command img path

--- src/views/ComboView.vue
+++ src/views/ComboView.vue
@@ -18,6 +18,10 @@ const combo = ref<string>('')
 onMounted(async () => {
   character.value = await getCharacterById(Array.isArray(route.params.characterId) ? route.params.characterId[0] : route.params.characterId) ?? null
 })
+
+const getCommandImgPath = (key: string) => {
+  return new URL(`/src/assets/command/${key}.png`, import.meta.url).href;
+}
 </script>
 
 <template>
@@ -37,32 +41,31 @@ onMounted(async () => {
     </div>
     <div v-else class="input input-borderd w-full">
       <div v-if="combo.length > 0" class="h-full flex items-center space-x-1">
-        <img v-for="key in combo?.trim().split(' ')" :key="key" :src="`/src/assets/command/${key}.png`"
-          class="h-[60%] aspect-square object-contain pointer-events-none select-none" />
+        <img v-for="key in combo?.trim().split(' ')" :key="key" :src="getCommandImgPath(key)"
+          class="h-5 aspect-square object-contain pointer-events-none select-none" />
       </div>
     </div>
 
     <div class="flex flex-row space-x-4 justify-center">
       <div class="grid grid-cols-3 gap-4">
-        <div v-for="move in moveList" :key="move.key" @click="combo += `${move.key} `">
-          <img :src="`/src/assets/command/${move.key}.png`"
-            class="btn btn-square object-contain p-2 pointer-events-none select-none" />
-        </div>
+        <button v-for="move in moveList" :key="move.key" @click="combo += `${move.key} `" class="btn btn-square">
+          <img :src="getCommandImgPath(move.key)" class="object-contain p-2 pointer-events-none select-none" />
+        </button>
       </div>
 
       <div class="grid grid-cols-4 gap-4">
-        <div v-for="attack in attackList" :key="attack.key" @click="combo += `${attack.key} `">
-          <img :src="`/src/assets/command/${attack.key}.png`"
-            class="btn btn-square object-contain p-2 pointer-events-none select-none" />
-        </div>
+        <button v-for="attack in attackList" :key="attack.key" @click="combo += `${attack.key} `"
+          class="btn btn-square">
+          <img :src="getCommandImgPath(attack.key)" class="object-contain p-2 pointer-events-none select-none" />
+        </button>
       </div>
 
 
       <div class="grid grid-cols-3 gap-4">
-        <div v-for="common in commonList" :key="common.key" @click="combo += `${common.key} `">
-          <img :src="`/src/assets/command/${common.key}.png`"
-            class="btn btn-square object-contain p-2 pointer-events-none select-none" />
-        </div>
+        <button v-for="common in commonList" :key="common.key" @click="combo += `${common.key} `"
+          class="btn btn-square">
+          <img :src="getCommandImgPath(common.key)" class="object-contain p-2 pointer-events-none select-none" />
+        </button>
       </div>
     </div>
   </div>
